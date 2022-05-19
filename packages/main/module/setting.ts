ac5c88c7d2780833da650cc5c2eda48d7942c318
Code pianist <1739616529@qq.com>
2022-05-19 12:19:32 +0800

    设置页面

--- packages/main/module/setting.ts
+++ packages/main/module/setting.ts
@@ -1,12 +1,29 @@
 import { useLoadWinPath, useWin } from '../lib/window'
+import { ipcMain } from 'electron'
+import { useLowDB } from '../config'
+const db = useLowDB()['option']
 const page_name = 'setting'
 function useSettingWin() {
 	const win = useWin(page_name)({
 		width: 500,
 		height: 500,
 		title: '设置',
+		titleBarStyle: 'hidden',
+		transparent: true,
+		visualEffectState: 'active',
+		vibrancy: 'hud', // fullscreen-ui header hud popover sidebar under-window
 	})
 	win.loadURL(useLoadWinPath(page_name))
+	win.webContents.openDevTools()
+
+	console.log(db.data)
+	ipcMain.on('setSetting', (e, data) => {
+		console.log(data)
+	})
+	ipcMain.handle('getSetting', (e, data) => {
+		console.log(data)
+	})
+	// win.setWindowButtonVisibility(true)
 }
 
 export { useSettingWin }
