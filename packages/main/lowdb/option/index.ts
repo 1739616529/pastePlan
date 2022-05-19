33586c1e022b48ccb535759ed2d6bbe8846c552a
Code pianist <1739616529@qq.com>
2022-05-19 12:18:03 +0800

    删除测试文件

--- packages/main/lowdb/option/index.ts
+++
@@ -1,6 +0,0 @@
-import { JSONFile, Low } from 'lowdb'
-import { resolve } from 'path'
-const json_file_path = resolve('db.json')
-console.error(json_file_path)
-const adapter = new JSONFile('db.json')
-const db = new Low(adapter)
