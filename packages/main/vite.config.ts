b8d60798553aa730e1f97d18532a3c81c48364bb
Code pianist <1739616529@qq.com>
2022-05-19 12:45:59 +0800

    node别名

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,31 +1,35 @@
+import { join } from 'path'
 import { builtinModules } from 'module'
 import { defineConfig } from 'vite'
 import esmodule from 'vite-plugin-esmodule'
 import pkg from '../../package.json'
-
 export default defineConfig({
-  root: __dirname,
-  plugins: [
-    esmodule([
-      'execa',
-    ]),
-  ],
-  build: {
-    outDir: '../../dist/main',
-    emptyOutDir: true,
-    minify: process.env./* from mode option */NODE_ENV === 'production',
-    sourcemap: true,
-    lib: {
-      entry: 'index.ts',
-      formats: ['cjs'],
-      fileName: () => '[name].cjs',
-    },
-    rollupOptions: {
-      external: [
-        'electron',
-        ...builtinModules,
-        ...Object.keys(pkg.dependencies || {}),
-      ],
-    },
-  },
+	root: __dirname,
+	plugins: [esmodule(['execa'])],
+	resolve: {
+		alias: {
+			main: join(__dirname),
+			project: join(__dirname, '../../'),
+		},
+	},
+	build: {
+		outDir: '../../dist/main',
+		emptyOutDir: true,
+		minify:
+			process.env./* from mode option */ NODE_ENV ===
+			'production',
+		sourcemap: true,
+		lib: {
+			entry: 'index.ts',
+			formats: ['cjs'],
+			fileName: () => '[name].cjs',
+		},
+		rollupOptions: {
+			external: [
+				'electron',
+				...builtinModules,
+				...Object.keys(pkg.dependencies || {}),
+			],
+		},
+	},
 })
