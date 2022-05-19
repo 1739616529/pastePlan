4eeccdaa26149bfc693d00142201fd0ef4085819
Code pianist <1739616529@qq.com>
2022-05-19 12:20:17 +0800

    preload

--- packages/preload/index.ts
+++ packages/preload/index.ts
@@ -6,9 +6,9 @@ import { useLoading } from './loading'
 const { appendLoading, removeLoading } = useLoading()
 
 ;(async () => {
-  await domReady()
+	await domReady()
 
-  appendLoading()
+	appendLoading()
 })()
 
 // --------- Expose some API to the Renderer process. ---------
@@ -18,19 +18,19 @@ contextBridge.exposeInMainWorld('ipcRenderer', withPrototype(ipcRenderer))
 
 // `exposeInMainWorld` can't detect attributes and methods of `prototype`, manually patching it.
 function withPrototype(obj: Record<string, any>) {
-  const protos = Object.getPrototypeOf(obj)
+	const protos = Object.getPrototypeOf(obj)
 
-  for (const [key, value] of Object.entries(protos)) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) continue
+	for (const [key, value] of Object.entries(protos)) {
+		if (Object.prototype.hasOwnProperty.call(obj, key)) continue
 
-    if (typeof value === 'function') {
-      // Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
-      obj[key] = function (...args: any) {
-        return value.call(obj, ...args)
-      }
-    } else {
-      obj[key] = value
-    }
-  }
-  return obj
+		if (typeof value === 'function') {
+			// Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
+			obj[key] = function (...args: any) {
+				return value.call(obj, ...args)
+			}
+		} else {
+			obj[key] = value
+		}
+	}
+	return obj
 }
