7ae3d9bd1fa922a47a452a18204eec16e5f5ed77
Code pianist <1739616529@qq.com>
2022-05-19 12:21:22 +0800

    设置页面

--- packages/renderer/src/page/Setting/Setting.tsx
+++ packages/renderer/src/page/Setting/Setting.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react"
+import { format_className } from "src/util/tools"
+import Icon from "src/components/Icon/Icon"
+import Form from "src/components/Form/Form"
+enum icons {
+	'icon-shezhi' = 'icon-shezhi',
+	"icon-kuaijiejian-" = 'icon-kuaijiejian-'
+}
+interface menu {
+	label: string
+	icon: icons
+}
+const menus: menu[] = [
+	{ label: '设置', icon: icons['icon-shezhi'] },
+	{ label: '快捷键', icon: icons["icon-kuaijiejian-"] },
+]
+
 function Setting() {
-	return (<div>Setting Page</div>)
+	const [active_menu, set_active_menu] = useState<icons>(menus[0].icon)
+	return (<div className="relative  select-none w-screen h-screen drag flex text-sm font-medium text-white" >
+		<div className="flex bg-black bg-opacity-40 flex-col justify-center items-center" style={{ "width": '69px' }}>
+
+
+			{
+				menus.map(v => (<div onClick={() => {
+					set_active_menu(v.icon)
+				}} key={v.icon} className={format_className("my-3 text-center", active_menu === v.icon ? 'text-blue-600' : '')}>
+					<Icon icon={v.icon} className="w-6 h-6 mx-auto " />
+					<span>{v.label}</span>
+				</div>))
+
+			}
+
+		</div>
+
+
+
+
+		<div className="flex-1 bg-black bg-opacity-60 flex justify-center items-center">
+			<div className={format_className("w-3/5 ", active_menu === icons['icon-shezhi'] ? 'block' : 'hidden')}>
+				<Form label-wrap={false}  >
+					<Form.Item label="开机启动： " ><input type="checkbox" /></Form.Item>
+				</Form>
+			</div>
+			<div className={format_className("w-3/5", active_menu === icons['icon-kuaijiejian-'] ? 'block' : 'hidden')}></div>
+		</div>
+	</div >)
 }
 
 export default Setting
