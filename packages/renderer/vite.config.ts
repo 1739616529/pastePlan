d9287dded7d6587ab41858078b1d84c6c831b319
Code pianist <1739616529@qq.com>
2022-05-19 12:20:26 +0800

    格式化

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -10,42 +10,44 @@ import pkg from '../../package.json'
  * @see https://vitejs.dev/config/
  */
 export default defineConfig({
-  mode: process.env.NODE_ENV,
-  root: __dirname,
-  plugins: [
-    react(),
-    electron(),
-    resolve({
-      /**
-       * Here you resolve some CommonJs module.
-       * Or some Node.js native modules they may not be built correctly by vite.
-       * At the same time, these modules should be put in `dependencies`,
-       * because they will not be built by vite, but will be packaged into `app.asar` by electron-builder
-       */
-      // ESM format code snippets
-      'electron-store': 'export default require("electron-store");',
-      // Node.js native module
-      serialport: `
+	mode: process.env.NODE_ENV,
+	root: __dirname,
+
+	plugins: [
+		react(),
+		electron(),
+		resolve({
+			/**
+			 * Here you resolve some CommonJs module.
+			 * Or some Node.js native modules they may not be built correctly by vite.
+			 * At the same time, these modules should be put in `dependencies`,
+			 * because they will not be built by vite, but will be packaged into `app.asar` by electron-builder
+			 */
+			// ESM format code snippets
+			'electron-store':
+				'export default require("electron-store");',
+			// Node.js native module
+			serialport: `
         const { SerialPort } = require("serialport");
         export { SerialPort }
       `,
-    }),
-  ],
-  base: './',
-  build: {
-    outDir: '../../dist/renderer',
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': join(__dirname, 'src'),
-    },
-  },
-  server: {
-    host: pkg.env.VITE_DEV_SERVER_HOST,
-    port: pkg.env.VITE_DEV_SERVER_PORT,
-  },
+		}),
+	],
+	base: './',
+	build: {
+		outDir: '../../dist/renderer',
+		emptyOutDir: true,
+		sourcemap: true,
+	},
+	resolve: {
+		alias: {
+			src: join(__dirname, 'src'),
+		},
+	},
+	server: {
+		host: pkg.env.VITE_DEV_SERVER_HOST,
+		port: pkg.env.VITE_DEV_SERVER_PORT,
+	},
 })
 
 /**
@@ -53,21 +55,21 @@ export default defineConfig({
  * @see https://github.com/caoxiemeihao/electron-vue-vite/issues/52
  */
 export function electron(
-  entries: Parameters<typeof optimizer>[0] = {}
+	entries: Parameters<typeof optimizer>[0] = {}
 ): Plugin {
-  const builtins = builtinModules.filter((t) => !t.startsWith('_'))
+	const builtins = builtinModules.filter((t) => !t.startsWith('_'))
 
-  /**
-   * @see https://github.com/caoxiemeihao/vite-plugins/tree/main/packages/resolve#readme
-   */
-  return optimizer({
-    electron: electronExport(),
-    ...builtinModulesExport(builtins),
-    ...entries,
-  })
+	/**
+	 * @see https://github.com/caoxiemeihao/vite-plugins/tree/main/packages/resolve#readme
+	 */
+	return optimizer({
+		electron: electronExport(),
+		...builtinModulesExport(builtins),
+		...entries,
+	})
 
-  function electronExport() {
-    return `
+	function electronExport() {
+		return `
 /**
  * For all exported modules see https://www.electronjs.org/docs/latest/api/clipboard -> Renderer Process Modules
  */
@@ -97,19 +99,22 @@ export {
   deprecate,
 }
 `
-  }
+	}
 
-  function builtinModulesExport(modules: string[]) {
-    return modules
-      .map((moduleId) => {
-        const nodeModule = require(moduleId)
-        const requireModule = `const M = require("${moduleId}");`
-        const exportDefault = `export default M;`
-        const exportMembers =
-          Object.keys(nodeModule)
-            .map((attr) => `export const ${attr} = M.${attr}`)
-            .join(';\n') + ';'
-        const nodeModuleCode = `
+	function builtinModulesExport(modules: string[]) {
+		return modules
+			.map((moduleId) => {
+				const nodeModule = require(moduleId)
+				const requireModule = `const M = require("${moduleId}");`
+				const exportDefault = `export default M;`
+				const exportMembers =
+					Object.keys(nodeModule)
+						.map(
+							(attr) =>
+								`export const ${attr} = M.${attr}`
+						)
+						.join(';\n') + ';'
+				const nodeModuleCode = `
 ${requireModule}
 
 ${exportDefault}
@@ -117,8 +122,8 @@ ${exportDefault}
 ${exportMembers}
 `
 
-        return { [moduleId]: nodeModuleCode }
-      })
-      .reduce((memo, item) => Object.assign(memo, item), {})
-  }
+				return { [moduleId]: nodeModuleCode }
+			})
+			.reduce((memo, item) => Object.assign(memo, item), {})
+	}
 }
