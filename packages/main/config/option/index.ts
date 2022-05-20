a62d58cd71854ef7f783fa530c99ce3339ccbe9f
Code pianist <1739616529@qq.com>
2022-05-20 08:55:41 +0800

    修改默认快捷键

--- packages/main/config/option/index.ts
+++ packages/main/config/option/index.ts
@@ -1,11 +1,11 @@
 import { JSONDB } from '../JSONDB'
-import { OptionDefaultData } from 'project/types/setting'
+import { OptionDefaultData, OptionData } from 'project/types/setting'
 const file_path = JSONDB.getJSONDBPath('option.json')
-const db = new JSONDB<OptionDefaultData>(file_path)
+const db = new JSONDB<OptionData>(file_path)
 // win		Ctrl		Win			Alt
 // mac		control   		command       Alt
 let default_data: OptionDefaultData = {
-	showHomeShortcut: ['alt', 'command', 'u'],
+	showHomeShortcut: ['Command', 'Alt', 'U'],
 	selfStart: false,
 }
 
