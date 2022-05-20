c6454638e211386c44b824acb9b9004495a9ec72
Code pianist <1739616529@qq.com>
2022-05-20 08:57:57 +0800

    防抖节流

---
+++ packages/util/tools.ts
@@ -0,0 +1,32 @@
+/** 节流 */
+export function throttle(fn: Function, delay: number = 500) {
+	let is_ok = true
+
+	return function (this: unknown, ...args: any[]) {
+		if (!is_ok) return
+		is_ok = false
+		fn.apply(this, args)
+
+		setTimeout(() => {
+			is_ok = true
+		}, delay)
+	}
+}
+
+/** 防抖 */
+export function debounce(fn: Function, delay: number = 500) {
+	let timer: number
+
+	return function (this: unknown, ...args: any[]) {
+		if (timer) clearTimeout(timer)
+		setTimeout(() => {
+			fn.apply(this, args)
+		}, delay)
+	}
+}
+
+export function is_have_key(obj: { [key: string]: any }, ...args: string[]) {
+	return args.some((v) => {
+		return obj[v] !== undefined
+	})
+}
