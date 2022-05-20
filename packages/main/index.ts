fe0f0e9d543c4c68f9d6a5bb986a5992d933dab8
Code pianist <1739616529@qq.com>
2022-05-20 08:55:12 +0800

    app.dock.hide

--- packages/main/index.ts
+++ packages/main/index.ts
@@ -6,17 +6,16 @@ import { useTrayMenu } from './module/tray'
 import './samples/npm-esm-packages'
 import { useSettingWin } from './module/setting'
 
+app.dock.hide()
 // Disable GPU Acceleration for Windows 7
 if (release().startsWith('6.1')) app.disableHardwareAcceleration()
 
 // Set application name for Windows 10+ notifications
 if (process.platform === 'win32') app.setAppUserModelId(app.getName())
-
 if (!app.requestSingleInstanceLock()) {
 	app.quit()
 	process.exit(0)
 }
-console.log(app.getLoginItemSettings())
 app.whenReady().then(() => {
 	// useMainWin()
 	useTrayMenu()
