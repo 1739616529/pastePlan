3427229746d453a0b8a2b5cf92f0e2ea5496af2f
Code pianist <1739616529@qq.com>
2022-05-21 13:23:34 +0800

    json DB 类修改

--- packages/main/config/JSONDB.ts
+++ packages/main/config/JSONDB.ts
@@ -1,5 +1,6 @@
 import { join, dirname } from 'path'
 import { app } from 'electron'
+
 import {
 	readFileSync,
 	writeFile,
@@ -9,12 +10,19 @@ import {
 	mkdirSync,
 } from 'fs-extra'
 
+export type BroadcastFun<T> = (data: Partial<T>) => void
+export type ListenList<T> = {
+	dataChange: BroadcastFun<T>[]
+}
+
 export class JSONDB<T> {
-	private _data: T | null = null
+	private _data!: T
 	private db_path: string = ''
 	private file_exist: boolean = false
 	public default_data: T | null = null
-
+	private listen_list: ListenList<T> = {
+		dataChange: [],
+	}
 	/** 传入 json文件的绝对路径 */
 	constructor(name: string) {
 		this.db_path = name
@@ -27,13 +35,17 @@ export class JSONDB<T> {
 		return this._data
 	}
 
-	public setData<V extends keyof T, Z extends T[V]>(data: {
-		[key in V]: Z
-	}) {
+	public setData(data: Partial<T>): this {
 		if (this._data) this._data = { ...this._data, ...data }
+		this.listen_list['dataChange'].forEach((v) => v(data))
 		return this
 	}
 
+	public on(cannle: keyof ListenList<T>, fn: BroadcastFun<T>) {
+		this.listen_list[cannle].push(fn)
+	}
+
+	/** 数据修改完必须 执行 write 才会保存本地 */
 	public write() {
 		const db_dir = dirname(this.db_path)
 		if (!existsSync(db_dir)) mkdirSync(db_dir)
