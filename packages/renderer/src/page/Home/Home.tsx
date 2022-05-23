7fad305b55548b6d2f01381e3165e446dede5611
Code pianist <1739616529@qq.com>
2022-05-23 14:11:07 +0800

    页面修改

--- packages/renderer/src/page/Home/Home.tsx
+++ packages/renderer/src/page/Home/Home.tsx
@@ -4,17 +4,23 @@ import Icon from "src/components/Icon/Icon"
 const { ipcRenderer } = window
 
 
-import { OptionData } from 'project/types/setting'
 import { format_className } from "src/util/tools"
 
+import { OptionDefaultData, option_item, OptionData } from 'project/types/setting'
+let def_option: OptionData = {
+	selfStart: false, showHomeShortcut: ['Control', 'E'], homeWinFixed: false
+}
 let db_option: OptionData
+
 function Home() {
 	const [plan_list, set_plan_list] = useState<PlanItem[]>([])
-	const [fixed_home, set_fixed_home] = useState<boolean>(false)
+
+	const [options, set_options] = useState<OptionData>(def_option)
 	useEffect(() => {
 		console.log('useEff')
 		ipcRenderer.removeAllListeners('home')
 		ipcRenderer.on('home', (e, { type, data }) => {
+			console.log(type, data)
 			if (type = 'clipboardData') set_plan_list(data)
 		})
 	}, [])
@@ -23,9 +29,21 @@ function Home() {
 	// 后端获取配置
 	if (!db_option) {
 		ipcRenderer.invoke('getSetting').then((val: OptionData) => {
-			set_fixed_home(val.homeWinFixed)
+			set_options(val)
+			console.log(val)
 			db_option = val
 		})
+	}
+
+	const set_options_fun: option_item = (data) => {
+		set_options((state) => {
+			return { ...state, ...data }
+		})
+
+	}
+
+	const set_option_to_db: option_item = (data) => {
+		ipcRenderer.send('setSetting', data)
 	}
 
 	function handle_plan_click(time: number) {
@@ -37,9 +55,12 @@ function Home() {
 
 
 
-			<div className=" flex mb-1 ">
-				<div className="rounded-md   hover:bg-gray-500  hover:bg-opacity-10  p-1 mr-1 ">
-					<Icon icon="icon-relieve-full" className={format_className(!fixed_home ? "text-blue-500 dark:text-blue-700 transform rotate-270" : '')} ></Icon>
+			<div className="flex mb-1" style={{ "zIndex": 999 }}>
+				<div onClick={() => {
+					set_option_to_db({ homeWinFixed: !options.homeWinFixed })
+					set_options_fun({ homeWinFixed: !options.homeWinFixed })
+				}} className="rounded-md   hover:bg-gray-500  hover:bg-opacity-10  p-1 mr-1 ">
+					<Icon icon="icon-relieve-full" className={format_className(options.homeWinFixed ? "text-blue-500 dark:text-blue-700 transform rotate-270" : '')} ></Icon>
 				</div>
 			</div>
 
@@ -48,11 +69,11 @@ function Home() {
 
 
 
-			<div className="flex-1 overflow-y-auto hidden-scroll-bar px-2 py-1">
+			<div className="flex-1 no-drag overflow-y-auto hidden-scroll-bar px-2 py-1">
 				{plan_list.map(v => (
 					<div onClick={() => {
 						handle_plan_click(v.time)
-					}} key={v.time} className="no-drag w-full hover:ring-1 mb-2 rounded-lg bg-gray-300 h-24 dark:bg-gray-900  overflow-hidden" >
+					}} key={v.time} className=" w-full hover:ring-1 mb-2 rounded-lg bg-gray-300 h-24 dark:bg-gray-900  overflow-hidden" >
 						{v.type === 'text'
 							? <div className="p-2 w-full h-full" children={v.data}></div>
 							: v.type === 'image'
