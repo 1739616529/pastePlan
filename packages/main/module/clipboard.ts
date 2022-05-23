98cbbfe249e52665e4e0393af35f53646ba8e4b0
Code pianist <1739616529@qq.com>
2022-05-23 11:10:46 +0800

    clipboard module 修改

--- packages/main/module/clipboard.ts
+++ packages/main/module/clipboard.ts
@@ -1,77 +1,91 @@
+import { join } from 'path'
 import { PlanItem } from 'project/types/planList.d'
 import { clipboard, NativeImage } from 'electron'
-let timer: NodeJS.Timer
+import { exec, ChildProcess } from 'child_process'
+import { EventEmitter } from 'events'
 let plan_list: PlanItem[] = []
-const text = ''
-const img = 'data:image/png;base64,'
-let is_change = false
 let plan_item: PlanItem = {
 	type: 'text',
 	time: 0,
 	data: '',
 }
-
-let save_clipboard_item: PlanItem | undefined
-
+let saveToclipDate = 0
 let naiveImageEnum: { [key: string | number]: NativeImage } = {}
-export function useClipboard() {
-	timer = setInterval(() => {
+
+function saveToClipboardData(type: 'text' | 'image') {
+	const new_date = new Date().getTime()
+	if (new_date - saveToclipDate <= 300) return
+	plan_item.type = type
+	plan_item.time = new_date
+
+	if (type === 'text') {
 		const text_ret = clipboard.readText()
-		if (text !== text_ret) {
-			if (text_ret === plan_item.data) return
-			plan_item.time = new Date().getTime()
-			is_change = true
-			plan_item.type = 'text'
-			plan_item.data = text_ret
+		plan_item.data = text_ret
+	}
+
+	if (type === 'image') {
+		const img_ret = clipboard.readImage()
+		let img_Base64 = img_ret.toDataURL()
+		const { width, height } = img_ret.getSize()
+		const ratio = width / height
+		if (ratio > 2.875) {
+			img_Base64 = img_ret
+				.resize({ width: 100 })
+				// .resize({ width: 268 })
+				.toDataURL()
 		} else {
-			const img_ret = clipboard.readImage()
-			let img_Base64 = img_ret.toDataURL()
-			if (img_Base64 === img) return
-			plan_item.time = new Date().getTime()
-			is_change = true
-			plan_item.type = 'img'
-			const { width, height } = img_ret.getSize()
-			const ratio = width / height
-			if (ratio > 2.875) {
-				img_Base64 = img_ret
-					.resize({ width: 100 })
-					// .resize({ width: 268 })
-					.toDataURL()
-			} else {
-				img_Base64 = img_ret
-					.resize({ height: 60 })
-					// .resize({ height: 90 })
-					.toDataURL()
-			}
-
-			if (img_Base64 === plan_item.data) return
-			plan_item.data = img_Base64
-			naiveImageEnum[plan_item.time] = img_ret
+			img_Base64 = img_ret
+				.resize({ height: 60 })
+				// .resize({ height: 90 })
+				.toDataURL()
 		}
 
-		if (
-			save_clipboard_item &&
-			save_clipboard_item.data === plan_item.data
+		plan_item.data = img_Base64
+		naiveImageEnum[plan_item.time] = img_ret
+	}
+
+	if (plan_list.length >= 20) {
+		const item = plan_list.pop()
+		if (item && item.type === 'image') {
+			delete naiveImageEnum[item.time]
+		}
+	}
+	console.log(plan_item)
+	plan_list.unshift({ ...plan_item })
+}
+
+class ListerClipboard extends EventEmitter {
+	private _child: ChildProcess | null = null
+	listering() {
+		this._child = exec(
+			join(__dirname, '../renderer/go-module/main'),
+			(...args) => {
+				console.log(args)
+			}
 		)
-			return
 
-		if (is_change) {
-			if (plan_list.length >= 20) {
-				const item = plan_list.pop()
-				if (item && item.type === 'img') {
-					delete naiveImageEnum[item.time]
-				}
-			}
+		this._child.stdout?.on('data', (data) => {
+			this.emit('change', { type: data.replace('\n', '') })
+		})
+	}
 
-			plan_list.unshift({ ...plan_item })
-		}
-	}, 600)
+	kill() {
+		if (this._child) this._child.kill()
+		this._child = null
+	}
+}
+
+let listen_clipboard: ListerClipboard | null
+export function useClipboard() {
+	listen_clipboard = new ListerClipboard()
+	listen_clipboard?.listering()
+	listen_clipboard?.on('change', ({ type }) => {
+		saveToClipboardData(type)
+	})
 }
 
 export function unCliboard() {
-	if (timer) clearInterval(timer)
-	is_change = false
-	save_clipboard_item = undefined
+	listen_clipboard?.kill()
 }
 
 export function getClipBoardData() {
@@ -82,13 +96,13 @@ export function saveToClipboard(time: number) {
 	plan_list.some((v) => {
 		if (v.time !== time) return
 
-		if (v.type === 'img') {
+		if (v.type === 'image') {
 			clipboard.writeImage(naiveImageEnum[time])
 		}
 
 		if (v.type === 'text') clipboard.writeText(v.data)
 
-		save_clipboard_item = v
+		saveToclipDate = new Date().getTime()
 		return true
 	})
 }
