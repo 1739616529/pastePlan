0addeb214b15a45dc796aaf018e6b4aa0c1c76d8
Code pianist <1739616529@qq.com>
2022-05-23 11:09:34 +0800

    清理不合适插件

--- packages/renderer/public/kbevent/binding.gyp
+++
@@ -1,8 +0,0 @@
-{
-"targets": [
-    {
-      "target_name": "kbhook",
-      "sources": [ "kbhook.cc" ],
-    },
-  ]
-}
--- packages/renderer/public/kbevent/test.js
+++
@@ -1,9 +0,0 @@
-const kbevent = require('./kbevent')
-kbevent.catchKeys(
-	{
-		eventType: 'keydown', //Optional, default: 'keydown'
-		keys: ['CONTROL', 'C'], // required
-		catchAgain: true, // Optional, default: false
-	},
-	callback
-)
